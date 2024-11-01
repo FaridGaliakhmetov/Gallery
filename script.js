31c3f7dc271f9dc88aafa80ef5e803e2992b5478
galiakhmetovf@gmail.com <galiakhmetov.f@gmail.com>
2024-11-01 12:12:09 +0500

    add files of the project

---
+++ script.js
@@ -0,0 +1,22 @@
+function displayOne(){
+  const butOne = document.querySelector(".showImgDiv");
+  const imgOne = document.querySelector(".showImg");    
+  butOne.style.display="flex";
+  imgOne.src="https://cdn.pixabay.com/photo/2014/11/30/14/11/cat-551554_1280.jpg";
+}
+function displayOff(){
+  const butOff = document.querySelector(".showImgDiv");
+  butOff.style.display="none";
+}
+function displayTwo(){
+const butOne = document.querySelector(".showImgDiv");
+  const imgOne = document.querySelector(".showImg");    
+  butOne.style.display="flex";
+  imgOne.src="https://cdn.pixabay.com/photo/2021/10/27/19/09/cat-6748193_1280.jpg";
+}
+function displayThree(){
+const butOne = document.querySelector(".showImgDiv");
+  const imgOne = document.querySelector(".showImg");    
+  butOne.style.display="flex";
+  imgOne.src="https://cdn.pixabay.com/photo/2022/02/17/04/54/animal-7017939_640.jpg";
+}
